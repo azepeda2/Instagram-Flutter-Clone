fd6631d3e58f7738b9f8fbc8e8bce98e973a663e
Alejandro Zepeda <zepedaalex25@gmail.com>
2022-09-12 13:55:34 -0700

    added responsive implementation

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'package:instagram_clone_flutter/models/user.dart';
-import 'package:instagram_clone_flutter/resources/auth_methods.dart';
+import 'package:instagram_flutter_clone/models/user.dart';
+import 'package:instagram_flutter_clone/resources/auth_methods.dart';
 
 class UserProvider with ChangeNotifier { 
   User? _user;
